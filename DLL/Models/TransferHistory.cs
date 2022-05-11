781bc1a07ac36f21dab9390ada4071a3a78e98f6
BartOstrovv <78106780+BartOstrovv@users.noreply.github.com>
2022-05-11 20:17:14 +0300

    Add Models

---
+++ DLL/Models/TransferHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Models
+{
+    public class Transfer
+    {
+        public int Id { get; set; }
+        public PersonData Sender { get; set; }
+        public double Sum { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Comment { get; set; }
+    }
+    public class TransferHistory
+    {
+        public int Id { get; set; }
+        public List<Transfer> Transfers { get; set; }
+    }
+}
